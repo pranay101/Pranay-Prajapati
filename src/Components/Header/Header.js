05295e37d244cf19353dedd7f1163964bfa46013
pranay101 <pranayprajapati101@gmail.com>
2022-01-24 22:28:39 +0530

    Initial Commit, removed unnecessary files, initialize header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from "./Header.module.css"
+
+function Header() {
+  return <div>
+      <h3>Home</h3>
+      <h3>About</h3>
+      <h3>Projects</h3>
+      <h3>Contact</h3>
+  </div>;
+}
+
+export default Header;
