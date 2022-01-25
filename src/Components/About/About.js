af8f41873533c9aff862665aa751abc1f8697a84
pranay101 <pranayprajapati101@gmail.com>
2022-01-25 17:01:26 +0530

    Implemented progres bar on tech front

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,8 +4,41 @@ import SchoolIcon from "@mui/icons-material/School";
 import GolfCourseIcon from "@mui/icons-material/GolfCourse";
 import SportsBaseballIcon from "@mui/icons-material/SportsBaseball";
 import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import Tech from "./tech/Tech";
 
 const About = () => {
+  const tech_list = [
+    {
+      title: "Nodejs",
+      experience: 3,
+      grip: 90,
+    },
+    {
+      title: "Nodejs",
+      experience: 3,
+      grip: 90,
+    },
+    {
+      title: "Express",
+      experience: 3,
+      grip: 90,
+    },
+    {
+      title: "Reactjs",
+      experience: 1,
+      grip: 95,
+    },
+    {
+      title: "Mongodb",
+      experience: 3,
+      grip: 90,
+    },
+    {
+      title: "Redux",
+      experience: 3,
+      grip: 50,
+    },
+  ];
   return (
     <div className={classes.About}>
       <div className={classes.About__section}>
@@ -42,12 +75,13 @@ const About = () => {
         </div>
       </div>
       <div className={classes.About__section}>
-          <h1>Tech I Like to work with</h1>
-        <div className={classes.About__images}>
-          <img src="https://nodejs.org/static/images/logo.svg" alt="" />
-          <img src="https://nodejs.org/static/images/logo.svg" alt="" />
-          <img src="https://nodejs.org/static/images/logo.svg" alt="" />
-          <img src="https://nodejs.org/static/images/logo.svg" alt="" />
+        <h1>Tech I Like to work with</h1>
+        <div className={classes.About__tech}>
+            {
+                tech_list.map((tech,index) =>{
+                    return <Tech key={index} title={tech.title} experience={tech.experience} grip={tech.grip} />
+                })
+            }
         </div>
       </div>
     </div>
---
+++ src/Components/About/tech/Tech.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "./Tech.module.css";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+const Tech = ({ title, experience, grip }) => {
+ 
+  return (
+    <div className={classes.Tech}>
+      <div className={classes.Tech__info}>
+        <h2>{title}</h2>
+        <p>Been working with for about {experience} years now</p>
+      </div>
+      <div className={classes.Tech__progress}>
+      <CircularProgressbar value={grip} text={`${grip}%`} />;
+      </div>
+    </div>
+  );
+};
+
+export default Tech;
